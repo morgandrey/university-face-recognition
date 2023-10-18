038cf5e6f115180b031241d6e496595b32267685
Andrey <morgunof.andreyy@gmail.com>
2023-10-18 22:13:31 +0300

    Add db models

---
+++ UniversityFaceRecognitionApp/DataAccess/UniversityDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityFaceRecognitionApp.Models;
+
+namespace UniversityFaceRecognitionApp.DataAccess;
+
+public partial class UniversityDbContext : DbContext
+{
+    public UniversityDbContext()
+    {
+    }
+
+    public UniversityDbContext(DbContextOptions<UniversityDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=university_db;Username=postgres;Password=123");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("person_pk");
+
+            entity.ToTable("person");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Patronymic).HasColumnName("patronymic");
+            entity.Property(e => e.Photo).HasColumnName("photo");
+            entity.Property(e => e.Surname).HasColumnName("surname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ UniversityFaceRecognitionApp/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversityFaceRecognitionApp.Models;
+
+public partial class Person
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public string? Patronymic { get; set; }
+
+    public byte[] Photo { get; set; } = null!;
+}
